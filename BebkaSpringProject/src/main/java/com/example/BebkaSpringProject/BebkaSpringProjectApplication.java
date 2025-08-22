e84a29b46d2503ff585761ca996fa8e3512a58b6
seyma <seymaertgrl@gmail.com>
2025-08-22 15:40:28 +0300

    initial commit

---
+++ BebkaSpringProject/src/main/java/com/example/BebkaSpringProject/BebkaSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.BebkaSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebkaSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BebkaSpringProjectApplication.class, args);
+	}
+
+}
---
+++ BebkaSpringProject/src/test/java/com/example/BebkaSpringProject/BebkaSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BebkaSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BebkaSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
