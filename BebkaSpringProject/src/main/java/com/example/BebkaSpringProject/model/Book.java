fc60492426922b98141a6ff3c5038fed1292a936
seyma <seymaertgrl@gmail.com>
2025-08-31 00:24:03 +0300

    [SP-8]-model-relationship

--- BebkaSpringProject/src/main/java/com/example/BebkaSpringProject/model/Book.java
+++ BebkaSpringProject/src/main/java/com/example/BebkaSpringProject/model/Book.java
@@ -1,15 +1,15 @@
 package com.example.BebkaSpringProject.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.lang.reflect.Type;
+import java.util.List;
+
 @Data
 @Entity
 @AllArgsConstructor
@@ -17,11 +17,18 @@ import java.lang.reflect.Type;
 @Getter
 @Setter
 public class Book {
+
     @Id
     @GeneratedValue
     long id;
 
     String author;
     String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    @JsonBackReference
+    private Student student;
+
 
 }
--- BebkaSpringProject/src/main/java/com/example/BebkaSpringProject/model/Student.java
+++ BebkaSpringProject/src/main/java/com/example/BebkaSpringProject/model/Student.java
@@ -1,21 +1,30 @@
 package com.example.BebkaSpringProject.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "students",schema = "public")
 @AllArgsConstructor
 public class Student {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
 
     String name;
     String email;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<Book> books;
 
 }
